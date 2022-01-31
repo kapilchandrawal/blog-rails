6083c6774e8d713fc9f157f932318ab195dafb84
kapilchandrawal <kapilchandrawal001@gmail.com>
2022-01-31 16:49:39 +0530

    Final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::Base
-end
+class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  # snippet for brevity
